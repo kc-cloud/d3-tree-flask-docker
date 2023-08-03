9da8b4b35b5b50024a9ff18bdc1f14cd9a8f8f8f
kannanc <kc-cloud@gmail.com>
2023-08-03 05:48:04 -0400

    updated

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 docker build --tag d3-python-docker .
 
 docker run -p 5001:5000 d3-python-docker
+docker build --tag d3-python-docker .
+docker build --tag d3-python-docker .
--- app.py
+++ app.py
@@ -7,10 +7,58 @@ app = Flask(__name__)
 #Reading data
 ##data_df = pd.read_csv("static/data/customers.csv")
 ##demography_df = data_df[(data_df['demography']=="Yes").notnull()]
+es_data = [
+  {
+    "name": "nexus",
+    "parent": "null",
+    "children": [
+      {
+        "name": "dev",
+        "parent": "nexus",
+        "children": [
+          {
+            "name": "service-1",
+            "parent": "dev",
+
+            "children": [
+              {
+                "name": "mysql-db",
+                "parent": "service-1"
+              },
+              {
+                "name": "s3",
+                "parent": "service-1"
+              }
+            ]
+                      
+          },
+          {
+            "name": "service-2",
+            "parent": "dev"
+          }
+        ]
+      },
+      {
+        "name": "test",
+        "parent": "nexus",
+        "children": [
+          {
+            "name": "service-3",
+            "parent": "test"
+          },
+          {
+            "name": "service-4",
+            "parent": "test"
+          }
+        ]
+      }
+    ]
+  }
+]
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+   return render_template('index.html', data = es_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
